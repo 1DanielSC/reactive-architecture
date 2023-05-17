7f279c036df756984ecce89554e15d553ce352e5
Daniel Colao <daniel.colao.700@ufrn.edu.br>
2023-05-17 16:27:54 -0300

    testing schedulers

--- product-back/src/main/java/com/reactive/productback/service/ProductService.java
+++ product-back/src/main/java/com/reactive/productback/service/ProductService.java
@@ -11,6 +11,7 @@ import com.reactive.productback.repository.ProductRepository;
 
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
 
 @Service
 public class ProductService {
@@ -33,16 +34,25 @@ public class ProductService {
     public Mono<Product> save(Product entity){
         return findByName(entity.getName())
         .switchIfEmpty(Mono.just(new Product(null, entity.getName(), 0L, entity.getPrice())))
+        .doOnNext(e -> {
+            System.out.println(Thread.currentThread().toString());
+        })
         .flatMap(e -> {
+            System.out.println(Thread.currentThread().toString());
             e.setQuantity(e.getQuantity()+entity.getQuantity());
             return Mono.just(e);
         })
-        .flatMap(repository::save);
+        .doOnNext(e -> {
+            System.out.println(Thread.currentThread().toString());
+        })
+        .flatMap(repository::save)
+        .subscribeOn(Schedulers.boundedElastic());
     }
 
     public Mono<Product> findByName(String name){
         //return repository.findByName(name).publishOn(Schedulers.fromExecutorService(Executors.newVirtualThreadPerTaskExecutor()))
-        return repository.findByName(name);
+        return repository.findByName(name)
+        .subscribeOn(Schedulers.boundedElastic());
     }
 
     public Mono<Product> update(Product entity){        
