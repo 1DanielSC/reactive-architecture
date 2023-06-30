e9bfdc01b0269eb32e1833eaf7ff44369f281ef9
1danielsc <danielscolao@gmail.com>
2023-06-29 22:52:19 -0300

    feat: message queue com Redis

--- review-back/src/main/java/com/reactive/reviewback/service/ProductReviewService.java
+++ review-back/src/main/java/com/reactive/reviewback/service/ProductReviewService.java
@@ -4,9 +4,11 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 import org.redisson.api.LocalCachedMapOptions;
+import org.redisson.api.RBlockingDequeReactive;
 import org.redisson.api.RLocalCachedMapReactive;
 import org.redisson.api.RMapCacheReactive;
 import org.redisson.api.RedissonReactiveClient;
+import org.redisson.client.codec.StringCodec;
 import org.redisson.codec.TypedJsonJacksonCodec;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -44,6 +46,8 @@ public class ProductReviewService {
     @Autowired
     private ProductServiceClient productClient;
 
+    private RBlockingDequeReactive<String> msgQueue;
+
     private RMapCacheReactive<String, ProductReview> productReviewCache;
     private RMapCacheReactive<String, Review> reviewCache;
 
@@ -51,6 +55,9 @@ public class ProductReviewService {
     // private RLocalCachedMapReactive<String, Review> reviewCache;
 
     public ProductReviewService(RedissonReactiveClient client) {
+        this.msgQueue = client.getBlockingDeque("mensageria", StringCodec.INSTANCE);
+
+
         this.productReviewCache = client.getMapCache("/product-review/", new TypedJsonJacksonCodec(String.class, ProductReview.class));
         this.reviewCache = client.getMapCache("/review/", new TypedJsonJacksonCodec(String.class, ProductReview.class));
         
@@ -128,7 +135,21 @@ public class ProductReviewService {
             return productReviewUpdated;
         })
         .flatMap(repository::save)
-        .flatMap(this::updateOrSaveOnCache);
+        .flatMap(this::updateOrSaveOnCache)
+        .publishOn(Schedulers.boundedElastic())
+        .doOnSuccess(e -> {
+            msgQueue.add("O produto " + e.getProductName() + " foi avaliado!\n"
+            + "Nota atualizada: " + e.getRating())
+            .subscribe();
+        })
+        .doOnNext(e -> printConfirmationMessage());
+    }
+
+    private void printConfirmationMessage(){
+        msgQueue.takeElements()
+        .doOnNext(e -> System.out.println("\nMensagem:" + e))
+        .doOnError(System.out::println)
+        .subscribe();
     }
 
     private Mono<ProductReview> updateOrSaveOnCache(ProductReview entity){
