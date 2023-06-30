4996eec21e852a23b2ad5516ab17ade7d2ddf495
1danielsc <danielscolao@gmail.com>
2023-06-30 00:25:05 -0300

    adiciona anotações restantes

--- review-back/src/main/java/com/reactive/reviewback/ReviewBackApplication.java
+++ review-back/src/main/java/com/reactive/reviewback/ReviewBackApplication.java
@@ -2,9 +2,11 @@ package com.reactive.reviewback;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.web.reactive.config.EnableWebFlux;
 
 @EnableWebFlux
+@EnableCaching
 @SpringBootApplication
 public class ReviewBackApplication {
 
--- sales-back/src/main/java/com/reactive/salesback/SalesBackApplication.java
+++ sales-back/src/main/java/com/reactive/salesback/SalesBackApplication.java
@@ -2,7 +2,11 @@ package com.reactive.salesback;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.web.reactive.config.EnableWebFlux;
 
+@EnableWebFlux
+@EnableCaching
 @SpringBootApplication
 public class SalesBackApplication {
 
