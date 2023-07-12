44124c3e62bc6415a16b05c88897c6b6fbb06176
1danielsc <danielscolao@gmail.com>
2023-07-12 09:53:39 -0300

    refactor: separa controllers

--- gateway/src/main/java/com/example/gateway/controller/OrderController.java
+++ gateway/src/main/java/com/example/gateway/controller/OrderController.java
@@ -1,27 +1,25 @@
-package com.example.gateway;
+package com.example.gateway.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.stream.function.StreamBridge;
 import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.support.MessageBuilder;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.gateway.entity.Item;
 import com.example.gateway.entity.RequestItemDTO;
 
 @RestController
-@RequestMapping("api")
-public class GlobalController {
+@RequestMapping("order")
+public class OrderController {
     
     @Autowired
     private StreamBridge bridge;
 
-    @PostMapping("/order")
+    @PostMapping
     public ResponseEntity<?> createOrder(@RequestBody(required = true) RequestItemDTO dto){
 
         Message<RequestItemDTO> message  = MessageBuilder.withPayload(dto).build();
