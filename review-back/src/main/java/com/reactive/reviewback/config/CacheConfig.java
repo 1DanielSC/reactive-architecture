1ee34a0d93f366e0e99065b05d992e1a028ae0d7
Daniel Colao <daniel.colao.700@ufrn.edu.br>
2023-07-13 23:05:57 -0300

    remove cache

--- review-back/src/main/java/com/reactive/reviewback/config/CacheConfig.java
+++ review-back/src/main/java/com/reactive/reviewback/config/CacheConfig.java
@@ -1,20 +1,20 @@
 package com.reactive.reviewback.config;
 
-import org.redisson.Redisson;
-import org.redisson.api.RedissonReactiveClient;
-import org.redisson.config.Config;
+// import org.redisson.Redisson;
+// import org.redisson.api.RedissonReactiveClient;
+// import org.redisson.config.Config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class CacheConfig {
     
-    @Bean
-    public RedissonReactiveClient getClient(){
-        Config config = new Config();
-        config.useSingleServer()
-            .setAddress("redis://127.0.0.1:6379");
-            //.setPassword("admin");
-        return Redisson.create(config).reactive();
-    }
+    // @Bean
+    // public RedissonReactiveClient getClient(){
+    //     Config config = new Config();
+    //     config.useSingleServer()
+    //         .setAddress("redis://127.0.0.1:6379");
+    //         //.setPassword("admin");
+    //     return Redisson.create(config).reactive();
+    // }
 }
